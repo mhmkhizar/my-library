ff088135a782b9ebdab1f216181a94a6cf034ac4
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-05-18 15:10:28 +0500

    Implement Book constructor and addBookToLibrary function

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const myLibrary = [];
 
+function Book(title, author, pages, readStatus) {
+  if (!new.target) {
+    throw Error`You must use the 'new' operator to call the constructor.`;
+  }
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, readStatus) {
+  const book = new Book(title, author, pages, readStatus);
+  myLibrary.push(book);
+}
