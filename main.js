141d291858b78b7fc2757439af49e82494b67258
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-05-27 22:06:13 +0500

    Refactor handleBookManipulation function to improve book deletion and read status toggling logic

--- main.js
+++ main.js
@@ -14,15 +14,15 @@ const pagesInp = modalForm.querySelector(`#bookPageCount`);
 const readStatInp = modalForm.querySelector(`#bookReadStatus`);
 
 const ICONS = {
-  check: `<svg class="icon check-icon" id="checkIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  check: `<svg class="icon check-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <title>check-circle</title>
       <path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z"/>
     </svg>`,
-  uncheck: `<svg class="icon uncheck-icon" id="uncheckIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  uncheck: `<svg class="icon uncheck-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <title>radiobox-blank</title>
       <path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"/>
     </svg>`,
-  delete: `<svg class="icon delete-icon" id="deleteIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  delete: `<svg class="icon delete-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <title>trash-can-outline</title>
       <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/>
     </svg>`,
@@ -31,6 +31,28 @@ const ICONS = {
 showModalBtn.addEventListener(`click`, openModal);
 closeModalBtn.addEventListener(`click`, closeModal);
 addModalBtn.addEventListener(`click`, handleAddBook);
+tableBody.addEventListener(`click`, handleBookManipulation);
+
+function handleBookManipulation(e) {
+  const icon = e.target.closest(".icon");
+  if (!icon) return;
+
+  const iconCell = icon.closest(`td`);
+  const iconRow = icon.closest(`tr`);
+  const rowBookId = iconRow.dataset.bookId;
+  const bookToManipulate = myLibrary.find((book) => book.id === rowBookId);
+  const index = myLibrary.indexOf(bookToManipulate);
+
+  if (icon.classList.contains(`delete-icon`) && index !== -1) {
+    myLibrary.splice(index, 1);
+    iconRow.remove();
+  } else {
+    bookToManipulate.toggleReadStatus();
+    iconCell.innerHTML = bookToManipulate.readStatus
+      ? ICONS.check
+      : ICONS.uncheck;
+  }
+}
 
 function handleAddBook(e) {
   if (!titleInp.value || !authorInp.value || !pagesInp.value) return;
